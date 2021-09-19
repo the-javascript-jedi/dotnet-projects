1676b794d1ca1139b0ba107cb5bf1a6586c033ef
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-09-19 12:26:21 +0530

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PetStruct dog;
+            //referring the enum data type-PetType
+            dog.Type = PetType.Dog;
+            dog.HasFur = true;
+            dog.Legs = 4;
+
+            //Instantiate a class
+            PetClass duck=new PetClass();
+            //referring the enum data type-PetType
+            duck.Type = PetType.Duck;
+            duck.HasFur = false;
+            duck.Legs = 2;
+            Console.WriteLine("A "+dog.Type+" has "+dog.Legs+" Legs");
+            Console.WriteLine("A " +duck.Type + " has "+ duck.Legs + " Legs");
+            Console.ReadLine();
+        }       
+    }
+    //class is a reference type and needs to be instantiated before they can be used
+    class PetClass
+    {
+        public int Legs;
+        public PetType Type;
+        public string Name;
+        public bool HasFur;
+    }
+    //struct is a value type - we can declare it like any other type
+    struct PetStruct
+    {
+        public int Legs;
+        public PetType Type;
+        public string Name;
+        public bool HasFur;
+    } 
+    //enum - custom type
+    enum PetType
+    {
+        Dog,
+        Duck,
+    }
+
+}
---
+++ Mosh/vidly/vidly/Controllers/MoviesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+using vidly.ViewModels;
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek The View"
+            };
+            var Movies = new List<Movie>
+            {
+                new Movie{ Name = "Transformers" },
+                new Movie{ Name = "The pursuit of happyness" },
+            };
+            var customers = new List<Customer>
+            {
+                new Customer{ Name="Customer 1"},
+                new Customer{ Name="Customer 2"}
+            };
+            // create a view model
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Movies=Movies,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+        }
+
+        // GET: Movies/Movies
+        public ActionResult Movies()
+        {           
+            var Movies = new List<Movie>
+            {
+                new Movie{ Name = "Transformers" },
+                new Movie{ Name = "The pursuit of happyness" },
+            };           
+            // create a view model
+            var viewModel = new RandomMovieViewModel
+            {                
+                Movies = Movies,               
+            };
+
+
+            return View(viewModel);
+        }
+    }
+
+}
