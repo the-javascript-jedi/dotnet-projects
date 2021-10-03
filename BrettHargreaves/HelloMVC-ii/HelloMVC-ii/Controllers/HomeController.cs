a408b3836f6bfbea8983fcb5578bf655ad7ecc4f
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-10-03 22:41:14 +0530

    added scaffolding

--- BrettHargreaves/HelloMVC-ii/HelloMVC-ii/Controllers/HomeController.cs
+++ BrettHargreaves/HelloMVC-ii/HelloMVC-ii/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +10,27 @@ namespace HelloMVC_ii.Controllers
 {
     public class HomeController : Controller
     {
+        //Create Object Cache
+        ObjectCache cache = MemoryCache.Default;
+        //List of Customer
+        List<Customer> customers;
+
+        //Constructor which will be run whenever we hit any endpoint
+        public HomeController()
+        {
+            //check if anything is in memory
+            customers = cache["customers"] as List<Customer>;
+            //if no customers create an empty list
+            if (customers == null)
+            {
+                customers = new List<Customer>();
+            }           
+        }
+        public void SaveCache()
+        {
+            //save the customers list in memory
+            cache["customers"] = customers;
+        }
         public ActionResult Index()
         {
             return View();
@@ -41,20 +63,27 @@ namespace HelloMVC_ii.Controllers
             return View(customer);
         }
 
-        // Add Customer View
+        // Add Customer View - this controller will accept different types of calls to it
         public ActionResult AddCustomer()
         {
             return View();
         }
+        //scaffolding AddCustomer page use [HttpPost] decorator because overriding with the same function name (AddCustomer)
+        //- this controller will accept POST methods
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            customer.Id = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+            //Redirect to CustomerList action
+            return RedirectToAction("CustomerList");
+        }
+
 
         //CustomerList scaffolding
         public ActionResult CustomerList()
         {
-            List<Customer> customers = new List<Customer>();
-
-            customers.Add(new Customer() { Name = "Fred",Telephone="12345" });
-            customers.Add(new Customer() { Name = "Barney", Telephone = "098765" });
-
             return View(customers);
         }
 
