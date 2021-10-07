34bcd6359475f3f53d887d6b3527bf1701f32ca6
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-10-07 23:58:45 +0530

    Added Model Validation

--- BrettHargreaves/HelloMVC-ii/HelloMVC-ii/Controllers/HomeController.cs
+++ BrettHargreaves/HelloMVC-ii/HelloMVC-ii/Controllers/HomeController.cs
@@ -75,6 +75,11 @@ namespace HelloMVC_ii.Controllers
         [HttpPost]
         public ActionResult AddCustomer(Customer customer)
         {
+            //Enforce the model decorators specified in the Model
+            if (!ModelState.IsValid)
+            {
+                return View(customer);
+            }
             customer.Id = Guid.NewGuid().ToString();
             customers.Add(customer);
             SaveCache();
--- BrettHargreaves/HelloMVC-ii/HelloMVC-ii/Models/Customer.cs
+++ BrettHargreaves/HelloMVC-ii/HelloMVC-ii/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +10,10 @@ namespace HelloMVC_ii.Models
     public class Customer
     {
         public string Id { get; set; }
+        //Decorators for enforcing Model Validaton
+        [Required]
+        [StringLength(10,ErrorMessage ="Your string is too long!!")]
+        [DisplayName("Enter your name")]
         public string Name { get; set; }
         public string Telephone { get; set; }
     }
