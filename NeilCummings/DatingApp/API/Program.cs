7fa69ba23ad160bbc095d511cca6a95bbfc48553
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-08-31 22:09:05 +0530

    added https to both client and api

--- NeilCummings/DatingApp/API/Program.cs
+++ NeilCummings/DatingApp/API/Program.cs
@@ -19,8 +19,9 @@ builder.Services.AddDbContext<DataContext>(opt =>
 builder.Services.AddCors();
 
 var app = builder.Build();
-
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+// add CORS Middleware to allow website name
+// app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
