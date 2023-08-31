38580eb97e052e4e44293b3113981dd1b40788ba
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-08-31 19:04:18 +0530

    made api call in ui and added cors support in backend

--- NeilCummings/DatingApp/API/Program.cs
+++ NeilCummings/DatingApp/API/Program.cs
@@ -15,18 +15,22 @@ builder.Services.AddDbContext<DataContext>(opt =>
     // if intellisense is not present try ctrl + '.'
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+// add CORS Support
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 
