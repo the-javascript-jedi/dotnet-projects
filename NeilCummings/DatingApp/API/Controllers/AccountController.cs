5dddb0c7834c931b9e4e9a7c74449f95822a6277
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-05 17:38:25 +0530

    added dto and validation

--- NeilCummings/DatingApp/API/Controllers/AccountController.cs
+++ NeilCummings/DatingApp/API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using API.Controllers;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API;
 public class AccountController : BaseApiController
@@ -18,21 +19,36 @@ public class AccountController : BaseApiController
 
     [HttpPost("register")] //POST: api/account/register
     // ActionResult<AppUser> is the return type
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
-        //when we specify using in front of variable creation - it will do automatic garbage collection - for the HMACSHA512 because it implements the dispose method
-        using var hmac = new HMACSHA512();
-        // create a user
-        var user = new AppUser
+        if (await UserExists(registerDto.Username))
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
-            PasswordSalt = hmac.Key
-        };
-        _context.Users.Add(user);
-        // save changes to db
-        await _context.SaveChangesAsync();
-        //saved user will be sent as response to the api endpoint
-        return user;
+            return BadRequest("username is taken!!!");
+        }
+        else
+        {
+            //when we specify using in front of variable creation - it will do automatic garbage collection - for the HMACSHA512 because it implements the dispose method
+            using var hmac = new HMACSHA512();
+            // create a user
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            _context.Users.Add(user);
+            // save changes to db
+            await _context.SaveChangesAsync();
+            //saved user will be sent as response to the api endpoint
+            return user;
+        }
+    }
+
+    //check to see if username already exists
+    private async Task<bool> UserExists(string username)
+    {
+        // AnyAsync- will determine if the value exists in the list or not
+        // return true or false if username exists in the list
+        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
 }
---
+++ NeilCummings/DatingApp/API/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API;
+public class RegisterDto
+{
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
