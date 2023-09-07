af866368c3d1aa32fb3482b9b2e9be2de4912e54
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-07 18:54:09 +0530

    added login api

--- NeilCummings/DatingApp/client/src/app/nav/nav.component.ts
+++ NeilCummings/DatingApp/client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  model:any={};
+  loggedIn=false;
+  constructor(private accountService:AccountService){}
 
+  ngOnInit(){}
+
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next:(response)=>{
+        console.log("response",response);
+        this.loggedIn=true;
+      },
+      error:(error)=>{
+        console.log("error",error);
+      }
+    })
+    console.log("this.model",this.model);
+  }
 }
