7c73b6d7e0601b3f1eeacd629e997519842b655b
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-10 14:46:25 +0530

    added routing to app

--- NeilCummings/DatingApp/client/src/app/app-routing.module.ts
+++ NeilCummings/DatingApp/client/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+// default - first matched route wins
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'members',component:MemberListComponent},
+  {path:'members/:id',component:MemberDetailComponent},
+  {path:'lists',component:ListsComponent},
+  {path:'messages',component:MessagesComponent},
+  // Wildcard component - redirects to below when route not present in above list
+  {path:'**',component:HomeComponent,pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- NeilCummings/DatingApp/client/src/app/nav/nav.component.ts
+++ NeilCummings/DatingApp/client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,7 @@ export class NavComponent {
   model:any={};
   // currentUser$:Observable<User|null>=of(null)
   loggedIn=false;
-  constructor(public accountService:AccountService){}
+  constructor(public accountService:AccountService,private router:Router){}
 
   ngOnInit(){
     // set the currentUser$ observale from the logged in the 
@@ -23,9 +24,11 @@ export class NavComponent {
     this.accountService.login(this.model).subscribe({
       next:(response)=>{
         console.log("response",response);
+        this.router.navigateByUrl('/members');
       },
       error:(error)=>{
         console.log("error",error);
+        this.router.navigateByUrl('/');
       }
     })
     console.log("this.model",this.model);
