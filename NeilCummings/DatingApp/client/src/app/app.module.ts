dab43fcb80cb32ebc9055bb81c0b1dd0dfb97e7e
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-10 18:42:09 +0530

    added new bootstrap theme and added modules

--- NeilCummings/DatingApp/client/src/app/app.module.ts
+++ NeilCummings/DatingApp/client/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './home/register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -32,11 +30,9 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule, // required animations module
-    ToastrModule.forRoot({
-      positionClass:'toast-bottom-right'
-    }), // ToastrModule added
-    BsDropdownModule.forRoot()
+    BrowserAnimationsModule,
+    // imports the shared module
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
