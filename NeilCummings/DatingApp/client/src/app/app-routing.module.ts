5d0facc615e5b10acd0ad84a183f49ad242e7f5b
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-10 17:36:01 +0530

    added authguard

--- NeilCummings/DatingApp/client/src/app/app-routing.module.ts
+++ NeilCummings/DatingApp/client/src/app/app-routing.module.ts
@@ -5,14 +5,24 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 // default - first matched route wins
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'members',component:MemberListComponent},
-  {path:'members/:id',component:MemberDetailComponent},
-  {path:'lists',component:ListsComponent},
-  {path:'messages',component:MessagesComponent},
-  // Wildcard component - redirects to below when route not present in above list
+  // applying authguard for single route
+  // {path:'members',component:MemberListComponent,canActivate:[authGuard]},
+  // applying authguard for multiple child routes
+  {path:'',
+  runGuardsAndResolvers:'always',
+  canActivate:[authGuard],
+  children:[
+    {path:'members',component:MemberListComponent},
+    {path:'members/:id',component:MemberDetailComponent},
+    {path:'lists',component:ListsComponent},
+    {path:'messages',component:MessagesComponent},
+  ]
+},
+// Wildcard component - redirects to below when route not present in above list
   {path:'**',component:HomeComponent,pathMatch:'full'},
 ];
 
