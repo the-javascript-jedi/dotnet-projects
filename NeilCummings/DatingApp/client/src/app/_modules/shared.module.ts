93984bab1fa00fbf00f193700523f0aa1339e6d4
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-13 09:16:18 +0530

    added buggy controller for testing different server errors

--- NeilCummings/DatingApp/client/src/app/_modules/shared.module.ts
+++ NeilCummings/DatingApp/client/src/app/_modules/shared.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
