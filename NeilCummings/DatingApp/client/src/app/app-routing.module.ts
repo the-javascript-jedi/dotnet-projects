85d9c98d8c4ddf4b3e8bca237ab19bd416f09aa3
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-14 23:15:50 +0530

    added interceptor for error handling

--- NeilCummings/DatingApp/client/src/app/app-routing.module.ts
+++ NeilCummings/DatingApp/client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
 // default - first matched route wins
 const routes: Routes = [
   {path:'',component:HomeComponent},
@@ -22,6 +23,7 @@ const routes: Routes = [
     {path:'messages',component:MessagesComponent},
   ]
 },
+{path:'errors',component:TestErrorComponent},
 // Wildcard component - redirects to below when route not present in above list
   {path:'**',component:HomeComponent,pathMatch:'full'},
 ];
