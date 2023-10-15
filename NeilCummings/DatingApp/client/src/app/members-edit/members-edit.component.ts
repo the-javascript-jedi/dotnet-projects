9d9bdd29ae74d100fd50f4c485a7e1e7ca588ad2
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-10-15 19:36:11 +0530

    added member form edit

--- NeilCummings/DatingApp/client/src/app/members-edit/members-edit.component.ts
+++ NeilCummings/DatingApp/client/src/app/members-edit/members-edit.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Member } from '../_models/member';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 import { MembersService } from '../_services/members.service';
 import { take } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-members-edit',
@@ -13,8 +15,9 @@ import { take } from 'rxjs';
 export class MembersEditComponent {
   member:Member|undefined;
   user:User|null=null;
+  @ViewChild('editForm') editForm:NgForm|undefined;
 
-  constructor(private accountService:AccountService,private memberService:MembersService){
+  constructor(private accountService:AccountService,private memberService:MembersService,private toastr:ToastrService){
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next:user=>this.user=user
     })
@@ -29,5 +32,12 @@ export class MembersEditComponent {
     this.memberService.getMember(this.user.username).subscribe({
       next:member=>this.member=member
     })
+  }
+
+  updateMember(){
+    console.log("this.member",this.member);
+    this.toastr.success('Profile Updated Successfully');
+    // reset form
+    this.editForm?.reset(this.member);
   }
 }
