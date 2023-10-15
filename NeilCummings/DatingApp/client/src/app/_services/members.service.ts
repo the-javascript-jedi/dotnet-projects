402d9b5c3d9e8bb00c9b68738b7d803c0dbdf0db
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-10-15 22:57:16 +0530

    added api optimization

--- NeilCummings/DatingApp/client/src/app/_services/members.service.ts
+++ NeilCummings/DatingApp/client/src/app/_services/members.service.ts
@@ -2,24 +2,44 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl=environment.apiUrl;
+  // store api response in service and check if the stored data is not empty else make a new api call
+  members:Member[]=[];
+
   constructor(private http:HttpClient) { }
   // the auth token for the apis are added in the interceptor, so we do not need to specify it in the httpoptions
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+'users');
+    // if data exists return the existing data
+    if(this.members.length>0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl+'users').pipe(
+      map(
+        members=>{
+          this.members=members;
+          return members;
+        }
+      ));
   }
 
   getMember(username:string){
+    const member=this.members.find(x=>x.userName===username);
+    if(member) return of(member);
+
     return this.http.get<Member>(this.baseUrl+'users/'+username);
   }
 
   updateMember(member:Member){
-    return this.http.put(this.baseUrl+'users',member)
+    return this.http.put(this.baseUrl+'users',member).pipe(map(
+      ()=>{
+        const index=this.members.indexOf(member);
+        this.members[index]={...this.members[index],...member}
+      }
+    ))
   }  
   // // pass the authorization token
   // getHttpOptions(){
