c339e655c8a09a3d815c9ef0d88b51be34623d26
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-10-15 16:43:15 +0530

    added member detail css

--- NeilCummings/DatingApp/client/src/app/_modules/shared.module.ts
+++ NeilCummings/DatingApp/client/src/app/_modules/shared.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    TabsModule,
     ToastrModule.forRoot({
       positionClass:'toast-bottom-right'
     }), // ToastrModule added
@@ -14,7 +17,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
   ],
   exports:[
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ],
 })
 export class SharedModule { }
