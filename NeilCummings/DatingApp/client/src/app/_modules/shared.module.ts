ac437923a9e822cf4dfd11febd82b3ccdfb0662a
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-10-17 21:25:52 +0530

    added photo upload

--- NeilCummings/DatingApp/client/src/app/_modules/shared.module.ts
+++ NeilCummings/DatingApp/client/src/app/_modules/shared.module.ts
@@ -4,6 +4,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [],
@@ -16,13 +17,15 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     NgxSpinnerModule.forRoot({
       type:'line-scale-party'
     }),
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    FileUploadModule
   ],
   exports:[
     BsDropdownModule,
     ToastrModule,
     TabsModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    FileUploadModule
   ],
 })
 export class SharedModule { }
